d50c628edcf4c5ec9da058f46c54b9add16b08a3
alemonacoo <ale.monaco@outlook.com>
2022-08-25 15:47:08 +0200

    Adding Algorithm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 console.log("Js OK!");
 
 for (let i = 1; i <= 100; i++) {
-
+    switch(0){
+      case (i % 15):
+        console.log('FizBuz');
+        break;
+      case (i%3):
+        console.log('Fiz');
+        break;
+      case (i%5):
+        console.log('Buz');
+        break;
+       default:
+        console.log(i);
+        break; 
+      }
 
 }
